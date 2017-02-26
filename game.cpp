584b7d9a1fbefccb1e0420e49167dfbada79c9ab
Chris Underhill <cpu22@cam.ac.uk>
2017-02-26 19:38:14 +0000

    Added todo and removed unnecessary bool played from player

--- game.cpp
+++ game.cpp
@@ -3,17 +3,19 @@
 
 using namespace std;
 
+
+//Todo: give each player a unique randomly generated identifier upon connection
+//such that if a player disconnects they can reconnect by supplying their UID.
+
 class Player{
     //Player class will join up to the player's sockets somehow and take their
-    //input. Bool played needs resetting at the start of each round and stores
-    //not quite sure what's going on with play() and selectCard, we probably
-    //only need one.
+    //input. Player class may want to notify the game state that it has
+    //selected.
     private:
         int ID;
         int numCards;
         vector<bool> hand;
         int currentSelection;
-        bool played;
 
     public:
         vector<bool> getHand(){
@@ -24,18 +26,25 @@ class Player{
             return ID;
         }
 
+        // Play() will be called on all players simulatenously by the server
+        // once everyone has selected a card
         int play(){
-            played = false;
-            return currentSelection;
+            if (currentSelection > 0){
+                int temp = currentSelection;
+                currentSelection = -1;
+                return temp;
+            }
+            else{
+                return -1
+            }
         }
 
         bool hasPlayed(){
-            return played;
+            return ( currentSelection > 0) ;
         }
 
         void selectCard(int card){
             currentSelection = card;
-            played = true;
         }
 };
 
